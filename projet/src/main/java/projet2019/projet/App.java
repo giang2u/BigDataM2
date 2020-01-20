b4a201a2489e75d7e891ce57acf4d0ec918c16e9
giang2u <exlibur@live.fr>
2020-01-20 16:58:24 +0100

    fini

--- projet/src/main/java/projet2019/projet/App.java
+++ projet/src/main/java/projet2019/projet/App.java
@@ -1,10 +1,27 @@
 package projet2019.projet;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
+import org.apache.hadoop.yarn.webapp.hamlet.HamletSpec._;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.ml.fpm.FPGrowth;
+import org.apache.spark.ml.fpm.FPGrowthModel;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+import org.apache.spark.sql.types.ArrayType;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 import scala.Tuple2;
 
@@ -14,7 +31,7 @@ import scala.Tuple2;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
     	
     	
@@ -24,10 +41,21 @@ public class App
     		      .config("spark.master", "local")
     		      .getOrCreate();
 
-    	 // recupere toutes les lignes des fichiers txt dans cf
-    	 JavaRDD<String> lines = spark.read().textFile("EVC-TXT/cf/*.txt").javaRDD();
     	 
+    	 // -------------------Q1)----------------------------
     	 
+
+    	 Scanner sc = new Scanner(System.in);
+    	 String rep = " ";
+    	 do{ 
+    		 System.out.println("Veuillez saisir cf ou cp : ");
+    		 rep= sc.nextLine();
+    		 System.out.println(rep);
+    	 }
+    	 while(!rep.equals("cf") && !rep.equals("cp") ) ;
+    	
+		 JavaRDD<String> lines = spark.read().textFile("EVC-TXT/"+rep+"/*.txt").javaRDD();
+
 
      	 
     	 // separe tous les mots qui ont un espace et les stocke 
@@ -73,30 +101,48 @@ public class App
     	 
     	 
     	 // Q5) ------------------------------------------------
-    	 int i = 0;
+    	 
+    	 
+    	 
     	 File dir = new File("EVC-TXT/cf");
     	 List<Row> data = new ArrayList<Row>();
     	 for (File f : dir.listFiles()) {
     		 
-    		 System.out.println(i +  "   " + f.getPath());
     		 JavaRDD<String> stock = spark.read().textFile(f.getPath()).javaRDD();
-    		 
     		 JavaRDD<String> item = stock.flatMap(s -> Arrays.asList(s.split("[^a-zA-ZáàâäãéèêëíìîïóòôöõúùûüýÿÁÀÂÄÃÉÈÊËÍÌÎÏÓÒÔÖÕÚÙÛÜÝ]")).iterator())
 						 						.map(String::toLowerCase);
     		 
-    		 List<String> listItem = item.collect();
     		 
-    		 data.add( RowFactory.create(Arrays.asList( listItem ) ) ) ;
-    		 i++;
-    	 }
-    	 /*
-    	 for (Row r : data) {
-    		 System.out.println(r.toString() );
-    		 Thread.sleep(5000);
+    		 // enelevage des caractere vide
+    		 JavaRDD<String> item2 = item.filter( word -> !word.equals(""));
+    		 // Q6) ----------------------------------
+
+        	 // filtre : enleve tous les mots de stopwords dans chaque transaction
+        	 JavaRDD<String> itemFiltrer = item2.filter(word -> !listStopWord.contains(word));
+        	 
+
+    		 List<String> listItem = itemFiltrer.collect().stream().distinct().collect(Collectors.toList());
+    		 
+        	 
+    		 data.add( RowFactory.create( listItem ) ) ;
     	 }
     	 
-    	 */
     	 
-    	 // Q6) -------------------------
+    	 
+    	 // Q7) -------------------------
+    	 
+    	 StructType schema = new StructType(new StructField[] {new StructField("items", new ArrayType(DataTypes.StringType, true), false,Metadata.empty() ) } );
+    	 
+    	 Dataset<Row> datas = spark.createDataFrame(data, schema);
+    	 
+    	 
+    	 FPGrowthModel fpg = new FPGrowth().setItemsCol("items").setMinSupport(0.2).fit(datas);
+    	 fpg.freqItemsets().orderBy(functions.col("freq").desc()).show(false);
+    	 
+    	 
+    	 
+    		    
+        System.out.println( "Hello World!" );
     }
 }
+
