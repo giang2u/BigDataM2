3cf4705ee451989454f29776fdf3518b7e3d111c
Stephane <stephane.yvoz@gmail.com>
2020-01-29 11:20:31 +0100

    graph

--- projet/src/main/java/Graph.java
+++ projet/src/main/java/Graph.java
@@ -1,4 +1,5 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.*;
@@ -14,23 +15,24 @@ public class Graph {
         SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
         SparkSession spark = SparkSession.builder().appName("BigData").getOrCreate();
 
+        spark.sparkContext().setLogLevel("Error");
         //Question 2
         //On crée les vertices (airport) et les edges (route)
 
-        StructType airportSchema = new StructType().add("ID", "int").add("Name", "string").add("City", "string").add("Country", "string").add("IATA", "string").add("ICAO", "string")
+        StructType airportSchema = new StructType().add("id", "int").add("Name", "string").add("City", "string").add("Country", "string").add("IATA", "string").add("ICAO", "string")
                 .add("Latitude", "double").add("Longitude", "double").add("Altitude", "int").add("Timezone", "int")
                 .add("DST", "string").add("Tz database time zone", "string").add("Type", "string").add("Source", "string");
 
 
-        Dataset<Row> vertices = spark.read().option("mode", "DROPMALFORMED").schema(airportSchema).csv("src/main/resources/airports.dat");
+        Dataset<Row> vertices = spark.read().option("mode", "DROPMALFORMED").schema(airportSchema).csv("EVC-TXT/airports.dat");
 
         
 
-        StructType routeSchema = new StructType().add("Airline", "string").add("airlineID", "int").add("src", "string")
-                .add("sourceAirportID", "int").add("dst", "string").add("destinationAirportID", "int")
+        StructType routeSchema = new StructType().add("Airline", "string").add("airlineID", "int").add("sourceAirport", "string")
+                .add("src", "int").add("destinationAirport", "string").add("dst", "int")
                 .add("codeshare", "string").add("stops", "int").add("equipment", "string");
 
-        Dataset<Row> edges = spark.read().option("mode", "DROPMALFORMED").schema(routeSchema).csv("src/main/resources/routes.dat");
+        Dataset<Row> edges = spark.read().option("mode", "DROPMALFORMED").schema(routeSchema).csv("EVC-TXT/routes.dat");
         
         
         // Q3  ---------------------------
@@ -42,7 +44,7 @@ public class Graph {
         g.edges().show();
         
         
-        
+      /*  
         // Q5 ----------------------
         
        Dataset<Row> degree = g.degrees().orderBy(functions.col("degree").desc());
@@ -68,14 +70,19 @@ public class Graph {
        Outdegree.show();
        
        
-       Dataset<Row> fusion = Indegree.join(Outdegree, Indegree.col("id").equalTo(Outdegree.col("id")) );
-     
+       Dataset<Row> fusion = Indegree.join(Outdegree, "id");
        
-       //mot2.mapToPair( s -> s.swap() ).keys().reduce( (v1,v2) -> (v1+v2) );
+       Dataset<Row> calcul = fusion.withColumn("result", Indegree.col("inDegree").divide(Outdegree.col("outDegree")) )
+    		   .select("id","result").orderBy(functions.col("result").desc());
        
-       Dataset<Row> calcul = fusion.withColumn("yes", Indegree.col("inDegree").divide(Outdegree.col("outDegree") ) );
        
        calcul.show();
+       
+       // Q9 utiliser g.triangleCount().run()
+       */
+       Dataset<Row> nbtriangle = g.triangleCount().run();
+       nbtriangle.orderBy(functions.col("count").desc()).show();
+       
        
     }
 
