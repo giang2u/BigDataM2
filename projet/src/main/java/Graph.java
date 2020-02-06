a702aa5723871fde4a08fe64e1036aede9a94c34
giang2u <exlibur@live.fr>
2020-02-06 16:07:03 +0100

    q10 11 debut 12

--- projet/src/main/java/Graph.java
+++ projet/src/main/java/Graph.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.StructType;
 import org.graphframes.GraphFrame;
+
+import scala.Tuple2;
 
 public class Graph {
 
@@ -15,7 +18,9 @@ public class Graph {
         SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
         SparkSession spark = SparkSession.builder().appName("BigData").getOrCreate();
 
-        spark.sparkContext().setLogLevel("Error");
+        
+        /*
+         
         //Question 2
         //On crée les vertices (airport) et les edges (route)
 
@@ -24,7 +29,7 @@ public class Graph {
                 .add("DST", "string").add("Tz database time zone", "string").add("Type", "string").add("Source", "string");
 
 
-        Dataset<Row> vertices = spark.read().option("mode", "DROPMALFORMED").schema(airportSchema).csv("EVC-TXT/airports.dat");
+        Dataset<Row> vertices = spark.read().option("mode", "DROPMALFORMED").schema(airportSchema).csv("src/main/resources/airports.dat");
 
         
 
@@ -32,7 +37,7 @@ public class Graph {
                 .add("src", "int").add("destinationAirport", "string").add("dst", "int")
                 .add("codeshare", "string").add("stops", "int").add("equipment", "string");
 
-        Dataset<Row> edges = spark.read().option("mode", "DROPMALFORMED").schema(routeSchema).csv("EVC-TXT/routes.dat");
+        Dataset<Row> edges = spark.read().option("mode", "DROPMALFORMED").schema(routeSchema).csv("src/main/resources/routes.dat");
         
         
         // Q3  ---------------------------
@@ -44,7 +49,7 @@ public class Graph {
         g.edges().show();
         
         
-      /*  
+        
         // Q5 ----------------------
         
        Dataset<Row> degree = g.degrees().orderBy(functions.col("degree").desc());
@@ -69,21 +74,69 @@ public class Graph {
        Indegree.show();
        Outdegree.show();
        
+       // Q8 ----------------- 
        
-       Dataset<Row> fusion = Indegree.join(Outdegree, "id");
+       Dataset<Row> fusion = Indegree.join(Outdegree, Indegree.col("id").equalTo(Outdegree.col("id")) );
+     
        
-       Dataset<Row> calcul = fusion.withColumn("result", Indegree.col("inDegree").divide(Outdegree.col("outDegree")) )
-    		   .select("id","result").orderBy(functions.col("result").desc());
+       //mot2.mapToPair( s -> s.swap() ).keys().reduce( (v1,v2) -> (v1+v2) );
        
+       Dataset<Row> calcul = fusion.withColumn("yes", Indegree.col("inDegree").divide(Outdegree.col("outDegree") ) );
        
        calcul.show();
        
+       
+       
        // Q9 utiliser g.triangleCount().run()
-       */
+       
        Dataset<Row> nbtriangle = g.triangleCount().run();
        nbtriangle.orderBy(functions.col("count").desc()).show();
        
+       // Q10 --------------------------------------------
        
+       System.out.println("BONJOUR " + g.vertices().count() );
+       System.out.println("BONJOUR " + g.edges().count() );
+       
+       */
+       
+       // Q11 -----------------------------------------------
+       
+        StructType airportSchema = new StructType().add("id", "int").add("Name", "string").add("City", "string").add("Country", "string").add("IATA", "string").add("ICAO", "string")
+                .add("Latitude", "double").add("Longitude", "double").add("Altitude", "int").add("Timezone", "int")
+                .add("DST", "string").add("Tz database time zone", "string").add("Type", "string").add("Source", "string");
+
+
+        Dataset<Row> verticesv2 = spark.read().option("mode", "DROPMALFORMED").schema(airportSchema).csv("src/main/resources/airports.dat");
+        
+      
+        StructType routeSchemav2 = new StructType().add("src", "int").add("source", "string")
+                .add("dst", "int").add("dest", "string").add("dep_delay", "double")
+                .add("dep_delay_new", "double").add("dep_del15", "double");
+
+        Dataset<Row> edgesv2 = spark.read().option("mode", "DROPMALFORMED").option("header", "true").schema(routeSchemav2).csv("src/main/resources/routesv2.csv");
+        
+        
+        GraphFrame gv2 = new GraphFrame(verticesv2, edgesv2);
+        
+     
+        // 1 ---------------------------
+        
+        //gv2.edges().filter( "source = 'SFO'" ).orderBy(functions.col("dep_delay").desc()).show();
+        
+        
+        // 2 ------------------------------
+        
+        //gv2.edges().filter( "source = 'SEA'" ).filter(" dep_delay > 10").select("source","dest", "dep_delay").show();
+        
+        
+        // Q12 ---------------------------
+        
+        Dataset<Row> trajetSJS = gv2.find("(SFO)-[]->(JAC); (JAC)-[]->(SEA)");
+        //trajetSJS.filter("SFO.IATA = 'JAC'").show();
+        //trajetSJS.show();
+        //trajetSJS.distinct().show();
+       // .filter(" JAC.IATA = 'JAC'").filter(" SEA.IATA = 'SEA'")
     }
 
 }
+
